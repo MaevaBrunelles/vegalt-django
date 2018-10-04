c57815f70a9289b008a36707c9d0e27794669218
Maëva Brunelles <maeva.brunelles@corp.ovh.com>
2018-10-04 17:38:44 +0200

    Add index page + Template

--- altproduct/views.py
+++ altproduct/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 # Create your views here.
 def index(request):
-    return HttpResponse("First initialization")
+    return render(request, 'altproduct/index.html')
+
+
+def legal(request):
+    return render(request, 'altproduct/legal.html')
--- vegalt/settings.py
+++ vegalt/settings.py
@@ -18,7 +18,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
-print(os.environ)
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
@@ -38,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'altproduct.apps.AltproductConfig',
 ]
 
 MIDDLEWARE = [
