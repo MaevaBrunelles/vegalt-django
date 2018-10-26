8c82e1f7a91ea55f5ecfdc3d651d1077d0dcee2c
Maëva Brunelles <maeva.brunelles@corp.ovh.com>
2018-10-26 09:50:54 +0200

    Unit + integration tests for account feature

--- altproduct/tests.py
+++ altproduct/tests.py
@@ -1,32 +1,62 @@
+""" Unit tests + Integration tests """
+
 from django.test import TestCase
 from django.urls import reverse
 from django.contrib.auth.models import User
 
-### UNIT TEST ###
 
-class LoginTestCase(TestCase):
-    """ Login page returns 200 """
+class ViewsTestCase(TestCase):
+    """ Unit tests for views """
 
     def test_login_page_returns_200(self):
+        """ Login page returns 200 """
+
         response = self.client.get(reverse('altproduct:account_login'))
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, 200)    
 
-# Login page returns 404
+    def test_bad_url_returns_404(self):
+        """ Bad URL returns 404 """
 
-# Login page returns 500
+        response = self.client.get('badurl')
+        self.assertEqual(response.status_code, 404)
 
 
-### FUNCTIONNAL TEST ###
+class AccountTestCase(TestCase):
+    """ Integration tests for account feature """
 
-# Create User in database
-class CreateUserTestCase(TestCase):
+    def setUp(self):
+        """ Create an user in test database """
+
+        self.fake_user = User.objects.create_user(
+            username='FakeUser',
+            email='test@mail.com',
+            password='fake_password'
+        )
+
     def test_create_user_in_db(self):
-        user = User.objects.create_user(username="FakeUser", email="test@mail.com", password="fake_password")
-        fake_user = User.objects.get(username="FakeUser")
-        self.assertEqual(fake_user, user)
+        """ Get the user in database to verify if it's created """
 
-# Check logout
+        user = User.objects.get(username='FakeUser')
+        self.assertEqual(user, self.fake_user)
 
-# Account login
+    def test_good_login_returns_true(self):
+        """ Test login with good credentials """
 
-# Login error
+        login = self.client.login(username='FakeUser', password='fake_password')
+        self.assertTrue(login)
+
+    def test_bad_login_returns_error(self):
+        """ Test login with bad credentials. Login must return True or False  """
+
+        login = self.client.login(username='BadUser', password='fake_password')
+        self.assertFalse(login)
+
+    def test_logout_is_effective(self):
+        """
+        Test logout feature. When the client is login, a session is created
+        with an id for the authentificated user, and it disappeared when the client logout.
+        """
+
+        self.client.login(username='FakeUser', password='fake_password')
+        self.client.logout()
+        self.assertRaises(KeyError, lambda: self.client.session['_auth_user_id'])
