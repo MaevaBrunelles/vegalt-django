952af047bda3d5d167c0876616acaaa6833c2a87
Maëva Brunelles <maeva.brunelles@corp.ovh.com>
2019-01-24 17:26:19 +0100

    Set SMTP parameters for mail send feature

--- vegalt/settings/__init__.py
+++ vegalt/settings/__init__.py
@@ -14,6 +14,7 @@ import os
 
 import dj_database_url
 
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))) + "/altproduct/"
 
@@ -144,3 +145,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Email settings
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+EMAIL_HOST = os.getenv('EMAIL_HOST')
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+#DEFAULT_FROM_EMAIL = 'Dont Reply <vegalt@ovh.fr>'
+#DEFAULT_FROM_EMAIL = 'contact@vegalt.fr'
+#SERVER_EMAIL = 'vegalt@ovh.fr'
